2a45163d5c5afce96dd6bc5642ee5a8b2ba2c3d8
Magoninho <magoninhogamer@gmail.com>
2022-08-15 22:36:50 -0300

    fixed no image error

--- src/ImageUtils.ts
+++ src/ImageUtils.ts
@@ -1,9 +1,12 @@
 export default class ImageUtils {
 	public static loadImageFromUrl(url: string): Promise<HTMLImageElement> {
-		return new Promise(resolve => {
+		return new Promise((resolve, reject) => {
 			const img = new Image();
 			img.onload = () => {
 				resolve(img);
+			}
+			img.onerror = () => {
+				img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=";
 			}
 			img.src = url;
 		});
